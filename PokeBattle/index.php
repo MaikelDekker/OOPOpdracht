92e5c2daee2953e00afbce8839b3f5356bfc4852
Maikel <Maikel.dekker@gmail.com>
2018-11-12 13:33:54 +0100

    Update

--- PokeBattle/index.php
+++ PokeBattle/index.php
@@ -1,19 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Poke Battle</title>
+</head>
+<body>
+    <form method="post">
+        <h3>Pikachu name:</h3>
+        <input id="PikachuName" name="PikachuName" required>
+        <h3>Charmeleon name:</h3>
+        <input id="CharmeleonName" name="CharmeleonName" required>
+        <input type="submit" id="submit"></button>
+    </form>
+</body>
+</html>
 <?php
-require 'Pokemon.php';
-require 'Pikachu.php';
-require 'Charmeleon.php';
-
-$pikachu = new Pikachu('Pikachu');
-print_r('<pre>'. $pikachu . '</pre>');
-$charmeleon = new Charmeleon('Charmeleon');
-print_r('<pre>'. $charmeleon . '</pre>');
-
-print_r('<pre>'. $charmeleon->name . ' Health: ' . $charmeleon->health . '</pre>');
-print_r('<pre>'. $pikachu->name . ' Health: ' . $pikachu->health . '</pre>');
-print_r('<pre>'. $pikachu->name . ' Attack'. '</pre>');
-$pikachu->Attack($charmeleon, 'Electric Ring');
-print_r('<pre>'. $charmeleon->name . ' Health: ' . $charmeleon->health . '</pre>');
-print_r('<pre>'. $charmeleon->name . ' Attack'. '</pre>');
-$charmeleon->Attack($pikachu, 'Flare');
-print_r('<pre>'. $pikachu->name . ' Health: ' . $pikachu->health . '</pre>');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require 'Pokemon.php';
+    require 'Pikachu.php';
+    require 'Charmeleon.php';
+    $pikachu = new Pikachu($_POST['PikachuName']);
+    print_r('<pre>'. $pikachu . '</pre>');
+    $charmeleon = new Charmeleon($_POST['CharmeleonName']);
+    print_r('<pre>'. $charmeleon . '</pre>');
+    print_r('<pre>'. $charmeleon->name . ' Health: ' . $charmeleon->health . '</pre>');
+    print_r('<pre>'. $pikachu->name . ' Health: ' . $pikachu->health . '</pre>');
+    print_r('<pre>'. $pikachu->name . ' Attacks with Electric Ring</pre>');
+    $pikachu->Attack($charmeleon, 'Electric Ring');
+    print_r('<pre>'. $charmeleon->name . ' Health: ' . $charmeleon->health . '</pre>');
+    print_r('<pre>'. $charmeleon->name . ' Attacks with Flare'. '</pre>');
+    $charmeleon->Attack($pikachu, 'Flare');
+    print_r('<pre>'. $pikachu->name . ' Health: ' . $pikachu->health . '</pre>');
+}
 ?>
